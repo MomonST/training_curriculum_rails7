a82aa4e65f53a995a0d8bdf6dcac7ee73e645858
MomonST <posy-today6d@icloud.com>
2025-04-28 17:51:40 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -30,13 +30,20 @@ class CalendarsController < ApplicationController
     plans = Plan.where(date: @todays_date..@todays_date + 6)
 
     7.times do |x|
+      today_date = @todays_date + x
       today_plans = []
+
       plans.each do |plan|
-        today_plans.push(plan.plan) if plan.date == @todays_date + x
+        today_plans.push(plan.plan) if plan.date == today_date
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+
+      days = {
+        month: today_date.month,
+        date: today_date.day,
+        wday: wdays[today_date.wday],  # ← ここで曜日もハッシュに追加！
+        plans: today_plans
+      }
       @week_days.push(days)
     end
-
   end
 end
